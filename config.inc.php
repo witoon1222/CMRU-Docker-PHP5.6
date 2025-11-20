01caa654f9b2d87a66909b03cec4f08e2b730611
witoon1222 <111549054+witoon1222@users.noreply.github.com>
2025-11-20 18:45:51 +0700

    Fix phpmyadmin

--- config.inc.php
+++ config.inc.php
@@ -10,7 +10,7 @@ $cfg['Servers'][$i]['password'] = 'YOUR-PASSWORD';
 $cfg['Servers'][$i]['auth_type'] = 'cookie'; // ใช้การล็อกอินแบบ Cookie
 $cfg['Servers'][$i]['hide_db'] = '^(information_schema|performance_schema|mysql)$';
 
-$cfg['PmaAbsoluteUri'] = '/phpmyadmin/';
+
 /* End of servers configuration */
 $cfg['Servers'][$i]['hide_db'] = '^(information_schema)$';
 $cfg['Servers'][$i]['hide_db'] = '^(performance_schema|mysql)$';
